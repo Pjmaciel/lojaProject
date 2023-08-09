06e4f93ef4b3a47e30b0e3af906f6cf97a74efb6
Pjmaciel <pjmaciel_4@hotmail.com>
2023-08-09 01:06:20 -0300

    Retrieve data from database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,38 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement(); // cria a instancia do objeto Statement.
+
+            rs = st.executeQuery("select * from department");// Executar o comando no mySql
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 }
