73d9dbd0f2633414e354c9dfcc70972df77a16ce
Pjmaciel <pjmaciel_4@hotmail.com>
2023-08-09 17:43:44 -0300

    Configure Insert data in Mysql

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,56 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+        Connection conn = null;
+        PreparedStatement st = null;
         try {
             conn = DB.getConnection();
 
-            st = conn.createStatement(); // cria a instancia do objeto Statement.
+            // st = conn.prepareStatement("INSERT INTO seller" +
+            // "(Name, Email, BirthDate, BaseSalary,DepartmentId)" +
+            // "VALUES" +
+            // "(?, ?, ?, ?, ?)",
+            // Statement.RETURN_GENERATED_KEYS);
 
-            rs = st.executeQuery("select * from department");// Executar o comando no mySql
+            // st.setString(1, "Pedro PedeMoleque");
+            // st.setString(2, "pedropedemoleque@gmail.com");
+            // st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            // st.setDouble(4, 3000);
+            // st.setInt(5, 4);
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+            int rowsAffected = st.executeUpdate();// Resulta um numero inteiro dizendo quantas linhas foram alteradas
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();// Retorna o Resultset
+                while (rs.next()) { // pecorre o Resultset
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else {
+                System.out.println("No rown affected!");
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
 
+            // } catch (ParseException e) {
+            // e.printStackTrace();
         } finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
